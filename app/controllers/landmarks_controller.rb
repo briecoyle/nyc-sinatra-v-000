cf511c6d5eb0993af70d16a922f3aed5fcfcede2
Brie Coyle <brie.coyle@gmail.com>
2017-02-01 21:00:59 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,31 @@
 class LandmarksController < ApplicationController
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.all.find(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.all.find(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    redirect to '/landmarks'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.update(params[:id], params[:landmark])
+    redirect to "/landmarks/#{@landmark.id}"
+  end
 
 end
--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -1,3 +1,5 @@
 class Landmark <ActiveRecord::Base
-
+  belongs_to :figure
+  belongs_to :figure_titles
+  has_many :landmarks
 end
